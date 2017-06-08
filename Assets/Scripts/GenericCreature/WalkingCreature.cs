2cc4fa3a7efe8d506bb9f0b107bf018035031ed7
Paco Gomez Granados <pacogomezgranados@gmail.com>
2017-06-08 11:16:20 +0200

    Using same brackets style as the rest of the project, some restructing of scripts location

--- Assets/Scripts/GenericCreature/WalkingCreature.cs
+++ Assets/Scripts/GenericCreature/WalkingCreature.cs
@@ -19,32 +19,41 @@ public class WalkingCreature : MonoBehaviour {
 	private bool m_isTouchingGround = false;
 	private bool m_isFalling = false;
 
-	void Start () {
+	void Start ()
+	{
 		InitColliderObjects ();
 		m_layerMask = (1 << (LayerMask.NameToLayer("Cave Colliders")));
 		UpdateLocalTransformDirection ();
 		Walk ();
 	}
 
-	void FixedUpdate () {
-		if (!m_isFalling && !m_isTouchingGround) {
-			if (IsGrounded ()) {
+	void FixedUpdate ()
+	{
+		if (!m_isFalling && !m_isTouchingGround)
+		{
+			if (IsGrounded ())
+			{
 				SettleDown();
-			} else {
+			}
+			else
+			{
 				StartFalling ();
 			}
 		}
 	}
 
-	private void StartFalling() {
+	private void StartFalling()
+	{
 		m_isFalling = true;
 		EndMovement ();
 		m_rigidBody.isKinematic = false;
 		// TODO notify falling
 	}
 
-	private void StopFalling() {
-		if (m_isFalling) {
+	private void StopFalling()
+	{
+		if (m_isFalling)
+		{
 			m_isFalling = false;
 			m_rigidBody.isKinematic = true;
 			m_rigidBody.velocity = new Vector2 (m_rigidBody.velocity.x, 0f);
@@ -53,7 +62,8 @@ public class WalkingCreature : MonoBehaviour {
 		}
 	}
 		
-	private bool IsGrounded() {
+	private bool IsGrounded()
+	{
 		Vector2 leftFeetPosition = new Vector2(m_feetCollider.bounds.min.x, m_feetCollider.bounds.min.y);
 		Vector2 rightFeetPosition = new Vector2(m_feetCollider.bounds.max.x, m_feetCollider.bounds.min.y);
 
@@ -63,11 +73,13 @@ public class WalkingCreature : MonoBehaviour {
 		);
 	}
 
-	public void Walk() {
+	public void Walk()
+	{
 		m_rigidBody.velocity = new Vector2(m_faceDirection * m_walkSpeed, m_rigidBody.velocity.y);
 	}
 
-	public void SettleDown() {
+	public void SettleDown()
+	{
 		/* Check if we need to fall down */
 		Vector2 leftSource = new Vector2(m_feetCollider.bounds.min.x + 0.5f, m_feetCollider.bounds.min.y);
 		Vector2 rightSource = new Vector2(m_feetCollider.bounds.max.x - 0.5f, m_feetCollider.bounds.min.y);
@@ -79,32 +91,39 @@ public class WalkingCreature : MonoBehaviour {
 		m_feetCollider.enabled = true;
 		m_bodyCollider.enabled = true;
 
-		if ((hitLeft.collider != null) && (hitRight.collider != null)) {
+		if ((hitLeft.collider != null) && (hitRight.collider != null))
+		{
 			m_rigidBody.position = new Vector2(m_rigidBody.position.x, m_rigidBody.position.y - Mathf.Min(hitLeft.distance, hitRight.distance));
 		}
 	}
 
-	public void Stop() {
+	public void Stop()
+	{
 		EndMovement();
 		SettleDown();
 	}
 
-	public void MoveStepUp() {
+	public void MoveStepUp()
+	{
 		m_rigidBody.position = new Vector2(m_rigidBody.position.x, m_rigidBody.position.y + 1.0f);
 	}
 
-	void EndMovement() {
+	void EndMovement()
+	{
 		/* Round the coordinates so they are perfect pixel aligned */
 		m_rigidBody.position = new Vector2(Mathf.Round(m_rigidBody.position.x), Mathf.Round(m_rigidBody.position.y));
 		m_rigidBody.velocity = Vector2.zero;
 	}
 
-	private void HandleBodyCollision(Collision2D collision) {
+	private void HandleBodyCollision(Collision2D collision)
+	{
 		// Check if any of the contact points is in the direction
 		// of the current movement. If so we have collided against a wall,
 		// readjust position and end movement
-		for (int i = 0; i < collision.contacts.Length; ++i) {
-			if (m_faceDirection * (m_rigidBody.position.x - collision.contacts [i].point.x) < 0.0f) {
+		for (int i = 0; i < collision.contacts.Length; ++i)
+		{
+			if (m_faceDirection * (m_rigidBody.position.x - collision.contacts [i].point.x) < 0.0f)
+			{
 				m_rigidBody.position = new Vector2 (collision.collider.bounds.center.x -
 					m_faceDirection * (collision.collider.bounds.extents.x + m_bodyCollider.bounds.extents.x),
 					m_rigidBody.position.y);
@@ -125,40 +144,54 @@ public class WalkingCreature : MonoBehaviour {
 		}
 	}
 
-	private void ChangeDirection() {
+	private void ChangeDirection()
+	{
 		m_faceDirection = -1 * m_faceDirection;
 		UpdateLocalTransformDirection ();
 	}
 
-	private void UpdateLocalTransformDirection() {
+	private void UpdateLocalTransformDirection()
+	{
 		transform.localScale = new Vector2(m_initialDirection * m_faceDirection * Mathf.Abs(transform.localScale.x), transform.localScale.y);		
 	}
 
-	private void HandleFeetCollision(Collision2D collision) {
+	private void HandleFeetCollision(Collision2D collision)
+	{
 		m_isTouchingGround = true;
-		if (m_isFalling) {
+		if (m_isFalling)
+		{
 			StopFalling ();
 		}
-		if (CollisionIsHorizontal (collision)) {
+		if (CollisionIsHorizontal (collision))
+		{
 			MoveStepUp ();
 		}
 	}
 
-	private void HandleFeetCollisionStay(Collision2D collision) {
-		if (CollisionIsHorizontal (collision)) {
+	private void HandleFeetCollisionStay(Collision2D collision)
+	{
+		if (CollisionIsHorizontal (collision))
+		{
 			MoveStepUp ();
 		}
 	}
 
-	private bool CollisionIsHorizontal(Collision2D collision) {
-		foreach (ContactPoint2D contact in collision.contacts) {
-			if (contact.normal == Vector2.left) {
-				if(contact.point.x < (m_feetCollider.bounds.max.x - 0.5f)) {
+	private bool CollisionIsHorizontal(Collision2D collision)
+	{
+		// TODO this code could be more elegant
+		foreach (ContactPoint2D contact in collision.contacts)
+		{
+			if (contact.normal == Vector2.left)
+			{
+				if(contact.point.x < (m_feetCollider.bounds.max.x - 0.5f))
+				{
 					return true;
 				}
 			}
-			if (contact.normal == Vector2.right) {
-				if(contact.point.x > (m_feetCollider.bounds.min.x + 0.5f)) {
+			if (contact.normal == Vector2.right)
+			{
+				if(contact.point.x > (m_feetCollider.bounds.min.x + 0.5f))
+				{
 					return true;
 				}
 			}
@@ -166,11 +199,13 @@ public class WalkingCreature : MonoBehaviour {
 		return false;
 	}
 
-	private void HandleFeetCollisionExit(Collision2D collision) {
+	private void HandleFeetCollisionExit(Collision2D collision)
+	{
 		m_isTouchingGround = false;
 	}
 
-	private void InitColliderObjects() {
+	private void InitColliderObjects()
+	{
 		m_rigidBody = GetComponent<Rigidbody2D>();
 		GameObject feetCollider = transform.FindChild ("FeetCollider").gameObject;
 		m_feetCollider = feetCollider.GetComponent<BoxCollider2D>();
@@ -188,14 +223,18 @@ public class WalkingCreature : MonoBehaviour {
 		CheckAllComponentsArePresent ();
 	}
 
-	private void CheckAllComponentsArePresent() {
-		if (m_rigidBody == null) {
+	private void CheckAllComponentsArePresent()
+	{
+		if (m_rigidBody == null)
+		{
 			Debug.LogError("Walking creature needs a rigid body");
 		}
-		if (m_feetCollider == null) {
+		if (m_feetCollider == null)
+		{
 			Debug.LogError("Walking creature needs a feet collider");
 		}
-		if (m_bodyCollider == null) {
+		if (m_bodyCollider == null)
+		{
 			Debug.LogError("Walking creature needs a body collider");
 		}
 	}
